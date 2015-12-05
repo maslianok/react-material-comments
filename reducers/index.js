ce96dde7a8a34adbb19b0a9cb53e772d77756fb5
maslianok <maslianok@gmail.com>
2015-12-05 10:01:50 +0200

    Finished ui for Example 1

--- reducers/index.js
+++ reducers/index.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import { routerStateReducer as router } from 'redux-router';
+import {routerStateReducer as router} from 'redux-router';
 
 import {commentActions} from '../config/actionTypes';
 
@@ -35,36 +35,5 @@ const rootReducer = combineReducers({
   comments,
   router
 })
-
-
-
-// import * as ActionTypes from '../actions'
-// import merge from 'lodash/object/merge'
-// import { routerStateReducer as router } from 'redux-router'
-// import { combineReducers } from 'redux'
-
-// // Updates an entity cache in response to any action with response.entities.
-// function entities(state = { users: {}, repos: {} }, action) {
-//   if (action.response && action.response.entities) {
-//     return merge({}, state, action.response.entities)
-//   }
-
-//   return state
-// }
-
-// // Updates error message to notify about the failed fetches.
-// function errorMessage(state = null, action) {
-//   const { type, error } = action
-
-//   if (type === ActionTypes.RESET_ERROR_MESSAGE) {
-//     return null
-//   } else if (error) {
-//     return action.error
-//   }
-
-//   return state
-// }
-
-
 
 export default rootReducer
